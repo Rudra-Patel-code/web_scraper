3b15035be97cf6da7cc6f247eb22093b1139ae26
Rudra-Patel-code <Rudra-Patel-code>
2025-09-24 14:57:31 -0600

    initial commit

--- linear_regression.py
+++ linear_regression.py
@@ -1,18 +1,3 @@
-# ---
-# jupyter:
-#   jupytext:
-#     text_representation:
-#       extension: .py
-#       format_name: percent
-#       format_version: '1.3'
-#       jupytext_version: 1.17.3
-#   kernelspec:
-#     display_name: base
-#     language: python
-#     name: python3
-# ---
-
-# %%
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
@@ -22,4 +7,4 @@ from sklearn.datasets import fetch_california_housing
 
 dataset = fetch_california_housing()
 
-print(dataset.data)
+print(dataset.DESCR)
